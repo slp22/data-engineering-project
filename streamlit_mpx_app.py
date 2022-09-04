ce3f0308b521c8a0b19ac25df6ee0185c0e54fca
Sandra Paredes <sandralparedes@gmail.com>
2022-09-04 16:30:03 -0400

    Update streamlit_mpx_app.py

--- streamlit_mpx_app.py
+++ streamlit_mpx_app.py
@@ -9,15 +9,8 @@ import streamlit as st
 st.write('''
 # Monkeypox Tweets
 #''')
-#
-#
-## PART 2 - Markdown Syntax
-#
-#st.write(
-#'''
-#
-### PART 3 - Table
-#
+
+
 st.write(
 '''
 Tweet Text
@@ -28,40 +21,11 @@ tweets = tweets[['date', 'tweet']]
 st.dataframe(tweets)
 
 
-## PART 4 - Graphing and Buttons
-#
-#st.write(
-#'''
-#### Graphing and Buttons
-#Let's graph some of our data with matplotlib. We can also add buttons to add interactivity to our app.
-#'''
-#)
-#
-#fig, ax = plt.subplots()
-#
-#ax.hist(data['PRICE'])
-#ax.set_title('Distribution of House Prices in $100,000s')
-#
-#show_graph = st.checkbox('Show Graph', value=True)
-#
-#if show_graph:
-#     st.pyplot(fig)
-#
-#
-# PART 5 - Mapping and Filtering Data
-
 st.write(
 '''
-Mapping and Filtering Data
+State Case Counts
 '''
 )
-
-#map_data = pd.read_csv('map_data.csv')
-#
-#case_input = st.slider('Filter', int(map_data['cases'].min()), int(map_data['cases'].max()), 4000)
-#
-#case_filter = map_data['cases'] < case_input
-#st.map(map_data.loc[case_filter, ['lat', 'long']])
 
 data = pd.read_csv('map_data.csv')
 #data = data.rename(columns={'latitude': 'lat', 'longitude': 'lon'})
@@ -70,3 +34,5 @@ price_input = st.slider('Filter', int(data['cases'].min()), int(data['cases'].ma
 
 price_filter = data['cases'] < price_input
 st.map(data.loc[price_filter, ['lat', 'lon']])
+st.markdown('Source: CDC 2022 U.S. Map & Case Count')
+st.markdown('https://www.cdc.gov/poxvirus/monkeypox/response/2022/index.html')

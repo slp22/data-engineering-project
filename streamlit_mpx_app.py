659768e71e0be42446d5f2a7f66134596c25ef44
Sandra Paredes <sandralparedes@gmail.com>
2022-09-04 16:38:42 -0400

    Update streamlit_mpx_app.py

--- streamlit_mpx_app.py
+++ streamlit_mpx_app.py
@@ -13,7 +13,7 @@ st.write('''
 
 st.write(
 '''
-Tweet Text
+## Tweet Text
 ''')
 
 tweets = pd.read_csv('tweets.csv')
@@ -23,16 +23,21 @@ st.dataframe(tweets)
 
 st.write(
 '''
-State Case Counts
+## State Case Counts
 '''
 )
 
 data = pd.read_csv('map_data.csv')
-#data = data.rename(columns={'latitude': 'lat', 'longitude': 'lon'})
 
-price_input = st.slider('Filter', int(data['cases'].min()), int(data['cases'].max()), 3500 )
+input = st.slider('Filter', int(data['cases'].min()), int(data['cases'].max()), 3500 )
 
-price_filter = data['cases'] < price_input
-st.map(data.loc[price_filter, ['lat', 'lon']])
-st.markdown('[Source: CDC 2022 U.S. Map & Case Count](https://www.cdc.gov/poxvirus/monkeypox/response/2022/index.html)')
+filter = data['cases'] < input
+st.map(data.loc[filter, ['lat', 'lon']])
+st.markdown('Source: [CDC 2022 U.S. Map & Case Count](https://www.cdc.gov/poxvirus/monkeypox/response/2022/index.html)')
+
+st.title('Title')
+st.header('Header')
+st.subheader('Subheader')
+st.caption('This is a string that explains something above.')
+st.write(mpl_fig) : Displays a Matplotlib figure
 

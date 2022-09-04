49c7f1eccf9660864d8a428de370debab44d5aa6
Sandra Paredes <sandralparedes@gmail.com>
2022-09-04 16:41:17 -0400

    Update streamlit_mpx_app.py

--- streamlit_mpx_app.py
+++ streamlit_mpx_app.py
@@ -9,12 +9,16 @@ import streamlit as st
 st.write('''
 # Monkeypox Tweets
 #''')
+st.title('Title')
+
 
 
 st.write(
 '''
 ## Tweet Text
 ''')
+st.subheader('Subheader')
+
 
 tweets = pd.read_csv('tweets.csv')
 tweets = tweets[['date', 'tweet']]
@@ -26,18 +30,19 @@ st.write(
 ## State Case Counts
 '''
 )
+st.subheader('Subheader')
+
 
 data = pd.read_csv('map_data.csv')
 
 input = st.slider('Filter', int(data['cases'].min()), int(data['cases'].max()), 3500 )
+st.caption('Caption')
+
 
 filter = data['cases'] < input
 st.map(data.loc[filter, ['lat', 'lon']])
 st.markdown('Source: [CDC 2022 U.S. Map & Case Count](https://www.cdc.gov/poxvirus/monkeypox/response/2022/index.html)')
 
-st.title('Title')
-st.header('Header')
-st.subheader('Subheader')
-st.caption('This is a string that explains something above.')
+st.caption('Caption')
 #st.write(mpl_fig) : Displays a Matplotlib figure
 

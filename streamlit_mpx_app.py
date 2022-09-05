b3ee19b86a7d2f8d4deb73d2b5763afe4672c75b
Sandra Paredes <sandralparedes@gmail.com>
2022-09-05 16:37:59 -0400

    Update streamlit_mpx_app.py

--- streamlit_mpx_app.py
+++ streamlit_mpx_app.py
@@ -33,19 +33,55 @@ filter = data['cases'] < input
 st.map(data.loc[filter, ['lat', 'lon']])
 st.markdown('Source: [CDC 2022 U.S. Map & Case Count](https://www.cdc.gov/poxvirus/monkeypox/response/2022/index.html)')
 
+st.write('''# cloud 1''')
 
 
 # Create some sample text
 cloud = pd.read_csv('words_for_cloud.csv')
 
 # Create and generate a word cloud image:
-wordcloud = WordCloud().generate(text)
+wordcloud = WordCloud().generate(cloud)
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 st.pyplot()
+
+
+st.write('''# cloud 2''')
+
+
+# Create text
+topic1 = 'emergency, global, pandemic, spreading, biden, cdc, said, risk, even, well, may, day, right, coming, could
+'
+topic2 = 'vaccine, smallpox, vaccines, cdc, shingles, day, well, im, yet, even, biden, moneypox, make, states, dont
+'
+topic3 = 'gay, men, sex, cnn, dont, spreading, stop, say, community, aids, right, cdc, see, pandemic, man
+'
+
+topic = st.selectbox('select topic',['topic1','topic2','topic3'])
+
+# Create and generate a word cloud image:
+def create_wordcloud(topic):
+    if topic == 'topic1':
+        topic = topic1
+    elif topic == 'topic2':
+        topic = topic2
+    else:
+        topic = topic3
+
+    wordcloud = WordCloud().generate(topic)
+    return wordcloud
+
+wordcloud = create_wordcloud(topic)
+
+# Display the generated image:
+fig, ax = plt.subplots(figsize = (12, 8))
+ax.imshow(wordcloud)
+plt.axis("off")
+st.pyplot(fig)
+
 
 
 

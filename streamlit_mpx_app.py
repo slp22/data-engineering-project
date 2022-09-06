34d3339cc62e6049f26d943f7a2be46082f9acd4
Sandra Paredes <sandralparedes@gmail.com>
2022-09-06 17:58:33 -0400

    Update streamlit_mpx_app.py

--- streamlit_mpx_app.py
+++ streamlit_mpx_app.py
@@ -23,38 +23,38 @@ with col1:
     st.metric(label="Handles", value="15,990")
 
 # ----Topic Word Cloud----
-# with col1:
-st.subheader("Topic Word Cloud")
+with col2:
+    st.subheader("Topic Word Cloud")
 
-topic1 = 'emergency, global, spreading, cdc, biden, pandemic, said, risk, day, yet, even, may, could, government, coming, children, fear, foxnews, states, stop'
-topic2 = 'vaccine, smallpox, cdc, vaccines, shingles, yet, day, states, risk, everyone, vax, make, foxnews, biden, also, many, even, government, really, since'
-topic3 = 'gay, men, sex, community, spreading, aids, cdc, stop, contact, man, children, month, risk, say, way, said, anyone, everyone, since, many'
-topic4 = 'cnn, biden, trump, real, democrats, states, make, msnbc, good, years, man, please, said, since, day, never, let, right, say, even'
-topic5 = 'dont, pandemic, see, moneypox, well, im, still, right, say, vaccines, even, thing, back, way, good, really, stop, biden, make, coming'
+    topic1 = 'emergency, global, spreading, cdc, biden, pandemic, said, risk, day, yet, even, may, could, government, coming, children, fear, foxnews, states, stop'
+    topic2 = 'vaccine, smallpox, cdc, vaccines, shingles, yet, day, states, risk, everyone, vax, make, foxnews, biden, also, many, even, government, really, since'
+    topic3 = 'gay, men, sex, community, spreading, aids, cdc, stop, contact, man, children, month, risk, say, way, said, anyone, everyone, since, many'
+    topic4 = 'cnn, biden, trump, real, democrats, states, make, msnbc, good, years, man, please, said, since, day, never, let, right, say, even'
+    topic5 = 'dont, pandemic, see, moneypox, well, im, still, right, say, vaccines, even, thing, back, way, good, really, stop, biden, make, coming'
 
-topic = st.selectbox('Select topic:',['risk','vaccine','gay men', 'news', 'mix'])
+    topic = st.selectbox('Select topic:',['risk','vaccine','gay men', 'news', 'mix'])
 
-def create_wordcloud(topic):
-    if topic == 'risk':
-        topic = topic1
-    elif topic == 'vaccine':
-        topic = topic2
-    elif topic == 'gay men':
-        topic = topic3
-    elif topic == 'news':
-        topic = topic4
-    else:
-        topic = topic5
+    def create_wordcloud(topic):
+        if topic == 'risk':
+            topic = topic1
+        elif topic == 'vaccine':
+            topic = topic2
+        elif topic == 'gay men':
+            topic = topic3
+        elif topic == 'news':
+            topic = topic4
+        else:
+            topic = topic5
 
-    wordcloud = WordCloud().generate(topic)
-    return wordcloud
+        wordcloud = WordCloud().generate(topic)
+        return wordcloud
 
-wordcloud = create_wordcloud(topic)
+    wordcloud = create_wordcloud(topic)
 
-fig, ax = plt.subplots(figsize = (12, 12))
-ax.imshow(wordcloud)
-plt.axis("off")
-st.pyplot(fig)
+    fig, ax = plt.subplots(figsize = (12, 12))
+    ax.imshow(wordcloud)
+    plt.axis("off")
+    st.pyplot(fig)
 
 # ----Tweets by Date----
 @st.cache
@@ -62,14 +62,14 @@ def load_date_data():
     data = pd.read_csv('date_df.csv')
     return data
 
-with col3:
+with col2:
     st.subheader("Tweets by Date")
     date_data = load_date_data()
     date_df = pd.DataFrame(date_data, columns = ['date', 'tweets_per_day'])
     st.line_chart(date_df, x='date', y='tweets_per_day')
 
 # ----Tweet Text----
-with col2:
+with col1:
     st.subheader("Monkeypox Tweets")
 
     tweets = pd.read_csv('tweets.csv')
@@ -78,7 +78,7 @@ with col2:
 
 
 # ----State Case Counts----
-with col4:
+with col3:
     st.subheader("State Case Counts")
 
     data = pd.read_csv('state_cases_for_map.csv')

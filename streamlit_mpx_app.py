cedeada54951ba703407cdb145c8fdcb5667174e
Sandra Paredes <sandralparedes@gmail.com>
2022-09-06 18:47:58 -0400

    Update streamlit_mpx_app.py

--- streamlit_mpx_app.py
+++ streamlit_mpx_app.py
@@ -19,52 +19,52 @@ col1, col2, col3 = st.columns([1, 2, 2], gap="medium")
 # with col1:
 #     st.metric(label="Handles", value="15,990")
 
-# ----Topic Word Cloud----
-with col2:
-    st.write("### Topic Word Cloud")
-
-    topic1 = 'emergency, global, spreading, cdc, biden, pandemic, said, risk, day, yet, even, may, could, government, coming, children, fear, foxnews, states, stop'
-    topic2 = 'vaccine, smallpox, cdc, vaccines, shingles, yet, day, states, risk, everyone, vax, make, foxnews, biden, also, many, even, government, really, since'
-    topic3 = 'gay, men, sex, community, spreading, aids, cdc, stop, contact, man, children, month, risk, say, way, said, anyone, everyone, since, many'
-    topic4 = 'cnn, biden, trump, real, democrats, states, make, msnbc, good, years, man, please, said, since, day, never, let, right, say, even'
-    topic5 = 'dont, pandemic, see, moneypox, well, im, still, right, say, vaccines, even, thing, back, way, good, really, stop, biden, make, coming'
-    
-    topic = st.selectbox('Select a topic:',['risk','vaccine','gay men', 'news', 'mix'])
-
-    def create_wordcloud(topic):
-        if topic == 'risk':
-            topic = topic1
-        elif topic == 'vaccine':
-            topic = topic2
-        elif topic == 'gay men':
-            topic = topic3
-        elif topic == 'news':
-            topic = topic4
-        else:
-            topic = topic5
-
-        wordcloud = WordCloud().generate(topic)
-        return wordcloud
-
-    wordcloud = create_wordcloud(topic)
-
-    fig, ax = plt.subplots(figsize = (12, 12))
-    ax.imshow(wordcloud)
-    plt.axis("off")
-    st.pyplot(fig)
-
-# # ----Tweets by Date----
-# @st.cache
-# def load_date_data():
-#     data = pd.read_csv('date_df.csv')
-#     return data
-# # st.write("#")
+# # ----Topic Word Cloud----
 # with col2:
-#     st.write("#")
-#     st.write("### Tweets by Date")
-#     date_data = load_date_data()
-#     date_df = pd.DataFrame(date_data, columns = ['date', 'tweets_per_day'])
-#     st.line_chart(date_df, x='date', y='tweets_per_day')
+#     st.write("### Topic Word Cloud")
+
+#     topic1 = 'emergency, global, spreading, cdc, biden, pandemic, said, risk, day, yet, even, may, could, government, coming, children, fear, foxnews, states, stop'
+#     topic2 = 'vaccine, smallpox, cdc, vaccines, shingles, yet, day, states, risk, everyone, vax, make, foxnews, biden, also, many, even, government, really, since'
+#     topic3 = 'gay, men, sex, community, spreading, aids, cdc, stop, contact, man, children, month, risk, say, way, said, anyone, everyone, since, many'
+#     topic4 = 'cnn, biden, trump, real, democrats, states, make, msnbc, good, years, man, please, said, since, day, never, let, right, say, even'
+#     topic5 = 'dont, pandemic, see, moneypox, well, im, still, right, say, vaccines, even, thing, back, way, good, really, stop, biden, make, coming'
+    
+#     topic = st.selectbox('Select a topic:',['risk','vaccine','gay men', 'news', 'mix'])
+
+#     def create_wordcloud(topic):
+#         if topic == 'risk':
+#             topic = topic1
+#         elif topic == 'vaccine':
+#             topic = topic2
+#         elif topic == 'gay men':
+#             topic = topic3
+#         elif topic == 'news':
+#             topic = topic4
+#         else:
+#             topic = topic5
+
+#         wordcloud = WordCloud().generate(topic)
+#         return wordcloud
+
+#     wordcloud = create_wordcloud(topic)
+
+#     fig, ax = plt.subplots(figsize = (12, 12))
+#     ax.imshow(wordcloud)
+#     plt.axis("off")
+#     st.pyplot(fig)
+
+# ----Tweets by Date----
+@st.cache
+def load_date_data():
+    data = pd.read_csv('date_df.csv')
+    return data
+# st.write("#")
+with col2:
+    st.write("#")
+    st.write("### Tweets by Date")
+    date_data = load_date_data()
+    date_df = pd.DataFrame(date_data, columns = ['date', 'tweets_per_day'])
+    st.line_chart(date_df, x='date', y='tweets_per_day')
 
 
 # # ----Tweet Text----
